bd86d5d81d28bd5f934dd0b835e5196a3581ef73
Philip Ting <philipjting@yahoo.com>
2013-07-07 03:15:41 -0600

    added gcc part 2

---
+++ ch5/09-gcc-p2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# extract
+cd $LFS/sources
+tar -xjf gcc-4.7.2.tar.bz2
+cd gcc-4.7.2
+
+# from the book
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+
+# bootstrap omitted - x86
+
+# append files
+for file in \
+ $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+
+# extract other files
+tar -Jxf ../mpfr-3.1.1.tar.xz
+mv -v mpfr-3.1.1 mpfr
+tar -Jxf ../gmp-5.1.1.tar.xz
+mv -v gmp-5.1.1 gmp
+tar -zxf ../mpc-1.0.1.tar.gz
+mv -v mpc-1.0.1 mpc
+
+sed -i 's/BUILD_INFO=info/BUILD_INFO=/' gcc/configure
+
+mkdir -v ../gcc-build
+cd ../gcc-build
+
+CC=$LFS_TGT-gcc \
+AR=$LFS_TGT-ar                  \
+RANLIB=$LFS_TGT-ranlib          \
+../gcc-4.7.2/configure          \
+    --prefix=/tools             \
+    --with-local-prefix=/tools  \
+    --with-native-system-header-dir=/tools/include \
+    --enable-clocale=gnu        \
+    --enable-shared             \
+    --enable-threads=posix      \
+    --enable-__cxa_atexit       \
+    --enable-languages=c,c++    \
+    --disable-libstdcxx-pch     \
+    --disable-multilib          \
+    --disable-bootstrap         \
+    --disable-libgomp           \
+    --with-mpfr-include=$(pwd)/../gcc-4.7.2/mpfr/src \
+    --with-mpfr-lib=$(pwd)/mpfr/src/.libs
+
+make
+make install
+
+ln -sv gcc /tools/bin/cc
+
+# cleanup
+cd ..
+rm -rf gcc-4.7.2 gcc-build
