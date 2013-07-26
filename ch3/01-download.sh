0b28f993f42d45daa8631b547f2ca3e9f93109d8
Philip Ting <philipjting@yahoo.com>
2013-07-25 18:25:02 -0600

    added copying of md5sums to sources

--- ch3/01-download.sh
+++ ch3/01-download.sh
@@ -4,8 +4,10 @@ mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 wget -i wget-list -P $LFS/sources
 
+cp md5sums $LFS/sources
 pushd $LFS/sources
 md5sum -c md5sums > md5sums_log
+rm md5sums
 popd
 
 echo "You can view the log at $LFS/sources/md5sums_log"
